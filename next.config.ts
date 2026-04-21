4b0a46cc7f88c3dead64c1b27a2c408d5c280399
gabu322 <gabrieldallorto@hotmail.com>
2026-04-21 15:18:17 -0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: "/wynnhub", // exact repo name you'll use on GitHub
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
